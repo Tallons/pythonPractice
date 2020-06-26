844afd218ddffa586161596a303782e5a9bd8cea
Tallons <Tallons27@yahoo.com>
2020-06-25 22:38:26 -0600

    activity & clean up code

--- basics/loops.py
+++ basics/loops.py
@@ -63,4 +63,25 @@ while chosen_exit not in available_exits:
         print("Game Over")
         break
 
-print("you an found exit")
+else:
+    print("you an found exit")
+
+# --------------------------------------------------
+
+choice = "-"
+
+while choice != "0":
+
+    if choice in "12345":
+        print("You have selected {}".format(choice))
+    else:
+        print("Please choose your option from the list below: ")
+        print("1.\tLearn Python")
+        print("2.\tLearn Java")
+        print("3.\tGo swimming")
+        print("4.\tHave dinner")
+        print("5.\tGo to bed")
+        print("0.\tExit")
+
+    choice = input()
+
